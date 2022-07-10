531a8a9fc72a2cf578ed9c31d231c4c43beda509
Bruno Dias <dias.h.bruno@gmail.com>
2022-07-10 20:05:43 -0300

    [feature] first implementation.

---
+++ index.js
@@ -0,0 +1,89 @@
+const d = document;
+
+function patch(a, b, p) {
+  return a && b ?
+    (
+      a.update(b, p),
+      a.k !== b.k && (b.create(p), b) || a
+    ) :
+  a ?
+    a.update(b, p):
+    (b.create(p), b);
+}
+
+function patchList(a, b, p) {
+  let inner = Math.max(a.length, b.length);
+  const cs = [];
+  for (let i = 0; i < inner; i++) {
+    const u = patch(a[i], b[i], p);
+    u && cs.push(u);
+  }
+  return cs;
+}
+
+function applyAttr(n, m, b) {
+  for (let attr in b) {
+    m[attr] = b[attr] ?
+      (n.setAttribute(attr, b[attr]), b[attr]) :
+      n.removeAttribute(attr);
+  }
+}
+
+function manageListeners(n, m, b) {
+  for (let attr in b) {
+    m[attr] = b[attr] ?
+      (n.addEventListener(attr, b[attr]), b[attr]) :
+      (n.removeEventListener(attr, m[attr]), null);
+  }
+}
+
+function VNode(k, o, l, c) {
+  this.k = k, this.o = o, this.c = c, this.l = l, this.n = null;
+}
+
+const N = (k, o, l, c) => new VNode(k, o, l, c);
+
+const np = VNode.prototype;
+
+np.create = function(p) {
+  const { k, o, c, l } = this;
+  const el = d.createElement(k);
+  applyAttr(el, o, o);
+  manageListeners(el, l, l);
+  for (let ch of c) { ch.create(el); }
+  p.appendChild(el);
+  this.n = el;
+}
+
+np.update = function(b, p) {
+  if (!b) p.removeChild(this.n);
+  else {
+    const { o, c, l, n } = this;
+    applyAttr(n, o, b.o);
+    manageListeners(n, l, b.l);
+    this.c = patchList(c, b.c, n);
+  }
+}
+
+function VNodeText(s) {
+  if (!(this instanceof VNodeText)) return new VNodeText(s);
+  this.s = s, this.n = null;
+}
+
+const T = (s) => new VNodeText(s);
+
+const tp = VNodeText.prototype;
+
+tp.create = function(p) {
+  const c = d.createTextNode(this.s);
+  p.appendChild(c);
+  this.n = c;
+}
+
+tp.update = function(b, p) {
+  !b ? p.removeChild(this.n) : (this.n.innerText = (this.s = b.s));
+}
+
+module.exports = {
+  T, N, patch,
+}
---
+++ test.js
@@ -0,0 +1,97 @@
+/* @jest-environment jsdom */
+
+const { patch, T, N } = require('./index.js');
+
+const template = `<!DOCTYPE html><html><body /></html>`;
+
+describe('creating nodes', () => {
+  it('text node', () => {
+    let r1 = patch(null, T('text'), document.body);
+    expect(r1.s).toBe('text');
+    expect(r1.n).not.toBeNull();
+  });
+
+  it('node', () => {
+    let r1 = patch(null, N('div', {}, {}, []), document.body);
+    expect(r1.n.nodeName).toBe('DIV');
+    expect(r1.n.children.length).toBe(0);
+  });
+});
+
+describe('deleting nodes', () => {
+  it('text node', () => {
+    let r1 = patch(null, T('text'), document.body);
+    r1 = patch(r1, null, document.body);
+    expect(r1).toBeUndefined();
+  });
+
+  it('node', () => {
+    let r1 = patch(null, N('div', {}, {}, []), document.body);
+    r1 = patch(r1, null, document.body);
+    expect(r1).toBeUndefined();
+  });
+
+  it('children node', () => {
+    let n1 = N('div', {}, {}, [T('a')]);
+    let n2 = N('div', {}, {}, []);
+    let r1 = patch(null, n1, document.body);
+    r1 = patch(r1, n2, document.body);
+    expect(r1.c.length).toBe(0);
+  });
+});
+
+describe('updating nodes', () => {
+  it('text node', () => {
+    let r1 = patch(null, T('a'), document.body);
+    r1 = patch(r1, T('b'), document.body);
+
+    expect(r1.s).toBe('b');
+    expect(r1.n.innerText).toBe('b');
+  });
+
+  describe('rendering inner nodes', () => {
+    it('update text inner node', () => {
+      const n = (text) => N('div', {}, {}, [text]);
+      let r1 = patch(null, n(T('a')), document.body);
+      const text = r1.c[0].n;
+      expect(r1.c.length).toBe(1);
+      expect(text.textContent).toBe('a');
+      r1 = patch(r1, n(T('b')), document.body);
+      expect(text.innerText).toBe('b');
+    });
+
+    it('changing inner node from div to p', () => {
+      const n = (t) => N('div', {}, {}, [
+	N(t, {}, {}, [
+	  T('text')
+	]),
+      ]);
+      let r1 = patch(null, n('div'), document.body);
+      let a = r1.c[0].n;
+      expect(a.nodeName).toBe('DIV');
+      patch(r1, n('p'), document.body);
+      a = r1.c[0].n;
+      expect(a.nodeName).toBe('P');
+    });
+  });
+});
+
+describe('managing listeners', () => {
+  it('setting up a listener', () => {
+    const f = jest.fn();
+    const n = N('button', { 'class': 'btn' }, { 'click': f }, [T('text')]);
+    let r1 = patch(null, n, document.body);
+    document.querySelector('.btn').click();
+    expect(f).toHaveBeenCalled();
+  });
+
+  it('disabling a listener', () => {
+    const f = jest.fn();
+    const n1 = N('button', { 'class': 'btn' }, { 'click': f }, [T('text')]);
+    const n2 = N('button', { 'class': 'btn' }, { 'click': null }, [T('text')]);
+    let r1 = patch(null, n1, document.body);
+    r1 = patch(r1, n2, document.body);
+    document.querySelector('.btn').click();
+    expect(f).not.toHaveBeenCalled();
+  });
+});
